044e8990905c3e0b9faa7ed78caa68dddc2b263d
Brun04 <bruno.verchere04@orange.fr>
2021-01-14 16:50:01 +0100

    Ex2 and 3

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+
 name := "spark2-template"
 
 import sbtassembly.MergeStrategy
--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -35,4 +35,47 @@ object examples {
 
 
   }
+
+  def exec2(): Unit ={
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val popCom = spark.read
+      .option("multiline", false)
+      .option("mode", "PERMISSIVE")
+      .json("data/input/demographie_par_commune.json")
+    //popCom.show
+
+    popCom.select(sum($"Population")).show
+
+    val popDpt = popCom.groupBy($"Departement")
+      .sum("Population")
+      .orderBy($"sum(Population)".desc)
+      .withColumnRenamed("sum(Population)", "Population")
+    popDpt.show
+
+    val dptCsv = spark.read.csv("/home/formation/Documents/Spark/data/departements.txt")
+      .toDF("Nom", "Departement")
+
+    popDpt.join(dptCsv, "Departement").show
+  }
+
+  def exec3(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val toursDF = spark.read
+      .option("multiline", true)
+      .option("mode", "PERMISSIVE")
+      .json("data/input/tours.json")
+    toursDF.show
+
+    toursDF.groupBy("tourDifficulty").count().show
+
+    toursDF.select($"tourDifficulty", $"tourPrice").groupBy($"tourDifficulty")
+      .count()
+      .select(min($"tourPrice"), max($"tourPrice"), avg($"tourPrice"))
+      .show
+
+  }
 }
